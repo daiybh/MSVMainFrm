cb38eeeca1af1b515066fa06a0d7bf5c5f21f4c2
daiybh <daiybh@gmail.com>
2014-04-03 10:26:23 +0800

    Add

---
+++ MSVMainFrm/MSVMainFrm.cpp
@@ -0,0 +1,272 @@
+
+// MSVMainFrm.cpp : 定义应用程序的类行为。
+//
+
+#include "stdafx.h"
+#include "afxwinappex.h"
+#include "MSVMainFrm.h"
+#include "MainFrm.h"
+
+#include "ChildFrm.h"
+#include "MSVMainFrmDoc.h"
+#include "MSVMainFrmView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMSVMainFrmApp
+
+BEGIN_MESSAGE_MAP(CMSVMainFrmApp, CWinAppEx)
+	ON_COMMAND(ID_APP_ABOUT, &CMSVMainFrmApp::OnAppAbout)
+	// 基于文件的标准文档命令
+#ifdef use_mdi_Framewnd
+	ON_COMMAND(ID_FILE_NEW, &CWinAppEx::OnFileNew)
+#else
+	ON_COMMAND(ID_FILE_NEW_FRAME, &CMSVMainFrmApp::OnFileNewFrame)
+	ON_COMMAND(ID_FILE_NEW, &CMSVMainFrmApp::OnFileNew)
+#endif
+	ON_COMMAND(ID_FILE_OPEN, &CWinAppEx::OnFileOpen)
+END_MESSAGE_MAP()
+
+
+// CMSVMainFrmApp 构造
+
+CMSVMainFrmApp::CMSVMainFrmApp()
+{
+
+	m_bHiColorIcons = TRUE;
+
+	// TODO: 在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
+}
+
+// 唯一的一个 CMSVMainFrmApp 对象
+
+CMSVMainFrmApp theApp;
+
+
+// CMSVMainFrmApp 初始化
+
+BOOL CMSVMainFrmApp::InitInstance()
+{
+	// 如果一个运行在 Windows XP 上的应用程序清单指定要
+	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
+	//则需要 InitCommonControlsEx()。否则，将无法创建窗口。
+	INITCOMMONCONTROLSEX InitCtrls;
+	InitCtrls.dwSize = sizeof(InitCtrls);
+	// 将它设置为包括所有要在应用程序中使用的
+	// 公共控件类。
+	InitCtrls.dwICC = ICC_WIN95_CLASSES;
+	InitCommonControlsEx(&InitCtrls);
+
+	CWinAppEx::InitInstance();
+
+	// 初始化 OLE 库
+	if (!AfxOleInit())
+	{
+		AfxMessageBox(IDP_OLE_INIT_FAILED);
+		return FALSE;
+	}
+	AfxEnableControlContainer();
+	// 标准初始化
+	// 如果未使用这些功能并希望减小
+	// 最终可执行文件的大小，则应移除下列
+	// 不需要的特定初始化例程
+	// 更改用于存储设置的注册表项
+	// TODO: 应适当修改该字符串，
+	// 例如修改为公司或组织名
+	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+	LoadStdProfileSettings(4);  // 加载标准 INI 文件选项(包括 MRU)
+
+	InitContextMenuManager();
+
+	InitKeyboardManager();
+
+	InitTooltipManager();
+	CMFCToolTipInfo ttParams;
+	ttParams.m_bVislManagerTheme = TRUE;
+	theApp.GetTooltipManager()->SetTooltipParams(AFX_TOOLTIP_TYPE_ALL,
+		RUNTIME_CLASS(CMFCToolTipCtrl), &ttParams);
+
+	// 注册应用程序的文档模板。文档模板
+	// 将用作文档、框架窗口和视图之间的连接
+	CMultiDocTemplate* pDocTemplate;
+#ifdef use_mdi_Framewnd
+	pDocTemplate = new CMultiDocTemplate(IDR_MSVMainFrmTYPE,
+		RUNTIME_CLASS(CMSVMainFrmDoc),
+		RUNTIME_CLASS(CChildFrame), // 自定义 MDI 子框架
+		RUNTIME_CLASS(CMSVMainFrmView));
+#else
+	pDocTemplate = new CMultiDocTemplate(IDR_MSVMainFrmTYPE,
+		RUNTIME_CLASS(CMSVMainFrmDoc),
+		RUNTIME_CLASS(CMainFrame), // 自定义 MDI 子框架
+		RUNTIME_CLASS(CMSVMainFrmView));
+#endif
+	if (!pDocTemplate)
+		return FALSE;
+
+	AddDocTemplate(pDocTemplate);
+
+
+
+	// 创建主 MDI 框架窗口
+#ifdef use_mdi_Framewnd
+	CMainFrame* pMainFrame = new CMainFrame;
+
+	if (!pMainFrame || !pMainFrame->LoadFrame(IDR_MAINFRAME))
+	{
+		delete pMainFrame;
+		return FALSE;
+	}
+	m_pMainWnd = pMainFrame;
+
+
+#endif
+	// 分析标准外壳命令、DDE、打开文件操作的命令行
+	CCommandLineInfo cmdInfo;
+	ParseCommandLine(cmdInfo);
+	//if(cmdInfo.m_nShellCommand == CCommandLineInfo::FileNew)
+	//	cmdInfo.m_nShellCommand = CCommandLineInfo::FileNothing;
+	if (!ProcessShellCommand(cmdInfo))
+		return FALSE;
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+
+	// 主窗口已初始化，因此显示它并对其进行更新
+
+	return TRUE;
+}
+
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialog
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialog(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialog)
+END_MESSAGE_MAP()
+
+// 用于运行对话框的应用程序命令
+void CMSVMainFrmApp::OnAppAbout()
+{
+	CAboutDlg aboutDlg;
+	aboutDlg.DoModal();
+}
+
+// CMSVMainFrmApp 自定义加载/保存方法
+
+void CMSVMainFrmApp::PreLoadState()
+{
+	BOOL bNameValid;
+	CString strName;
+	bNameValid = strName.LoadString(IDS_EDIT_MENU);
+	ASSERT(bNameValid);
+	GetContextMenuManager()->AddMenu(strName, IDR_POPUP_EDIT);
+	bNameValid = strName.LoadString(IDS_EXPLORER);
+	ASSERT(bNameValid);
+	GetContextMenuManager()->AddMenu(strName, IDR_POPUP_EXPLORER);
+}
+
+void CMSVMainFrmApp::LoadCustomState()
+{
+}
+
+void CMSVMainFrmApp::SaveCustomState()
+{
+}
+
+// CMSVMainFrmApp 消息处理程序
+
+
+
+void CMSVMainFrmApp::OnFileNewFrame() 
+{
+	ASSERT(m_pDocTemplate != NULL);
+
+	CDocument* pDoc = NULL;
+	CFrameWnd* pFrame = NULL;
+	if (m_pDocTemplate != NULL)
+		pDoc = m_pDocTemplate->CreateNewDocument();
+
+	if (pDoc != NULL)
+	{
+		pFrame = m_pDocTemplate->CreateNewFrame(pDoc, NULL);
+		if (pFrame != NULL)
+		{
+		
+			m_pDocTemplate->SetDefaultTitle(pDoc);
+			if (!pDoc->OnNewDocument())
+			{
+				pFrame->DestroyWindow();
+				pFrame = NULL;
+			}
+			else
+			{
+				m_pDocTemplate->InitialUpdateFrame(pFrame, pDoc, TRUE);
+			}
+		}
+	}
+
+
+	if (pFrame == NULL || pDoc == NULL)
+	{
+		delete pDoc;
+		AfxMessageBox(AFX_IDP_FAILED_TO_CREATE_DOC);
+	}
+}
+
+void CMSVMainFrmApp::OnFileNew() 
+{
+// #ifdef use_mdi_Framewnd
+// 	CWinAppEx::OnFileNew();
+// 	return;
+// #endif
+	CDocument* pDoc = NULL;
+	CFrameWnd* pFrame;
+	pFrame = DYNAMIC_DOWNCAST(CFrameWnd, CWnd::GetActiveWindow());
+
+	if (pFrame != NULL)
+		pDoc = pFrame->GetActiveDocument();
+
+	if (pFrame == NULL || pDoc == NULL)
+	{
+		CWinApp::OnFileNew();
+	}
+	else
+	{
+		if (!pDoc->SaveModified())
+			return;
+
+		CDocTemplate* pTemplate = pDoc->GetDocTemplate();
+		ASSERT(pTemplate != NULL);
+
+		if (pTemplate != NULL)
+			pTemplate->SetDefaultTitle(pDoc);
+		pDoc->OnNewDocument();
+	}
+}
