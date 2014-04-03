9e3457ea32c93b75803dfa5ee754a4bbb5cc1f09
daiybh <daiybh@gmail.com>
2014-04-03 13:46:49 +0800

    增加编译选项用来选择生成mdi ，sdi

--- MSVMainFrm/stdafx.h
+++ MSVMainFrm/stdafx.h
@@ -45,7 +45,6 @@
 
 #define MSVINFO_DBCLICK  WM_USER+100
 
-//#define use_mdi_Framewnd
 #ifdef _UNICODE
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
